c3b83a90f1594855c5ba4acf61900d7bccf92139
akinoringo <wbraver@outlook.jp>
2022-07-18 07:44:05 +0900

    feat: メモ追加機能の実装

--- simple_memo_laravel/app/Http/Controllers/MemoController.php
+++ simple_memo_laravel/app/Http/Controllers/MemoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Memo;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class MemoController extends Controller
@@ -12,6 +14,45 @@ class MemoController extends Controller
      */
     public function index()
     {
-        return view('memo');
+        $memos = Memo::where('user_id', Auth::id())->orderBy('updated_at', 'desc')->get();
+
+        return view('memo', [
+            'name' => $this->getLoginUserName(),
+            'memos' => $memos
+        ]);
+    }
+
+    /**
+     * メモの追加
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function add()
+    {
+        Memo::create([
+            'user_id' => Auth::id(),
+            'title' => '新規メモ',
+            'content' => '',
+        ]);
+
+        return redirect()->route('memo.index');
+    }
+
+    /**
+     * ログインユーザー名取得
+     * @return string
+     */
+    private function getLoginUserName() {
+        $user = Auth::user();
+
+        $name = '';
+        if ($user) {
+            if (7 < mb_strlen($user->name)) {
+                $name = mb_substr($user->name, 0, 7) . "...";
+            } else {
+                $name = $user->name;
+            }
+        }
+
+        return $name;
     }
 }
